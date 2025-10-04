758c4428030bc736ee8ebbd17c872ab00f2483b6
ValishaAngelina <angelinavalisha25@gmail.com>
2025-10-04 15:17:46 -0700

    dd

--- shaka/src/App.jsx
+++ shaka/src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <nav className="navbar">
+        <p>logo</p>
+        <div className="nav-container">
+          <ul className="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <h1>SHAKA</h1>
+
+      <section class="button-nav">
+        <a href='#'>ABOUT</a>
+        <a href='#'>START</a>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
